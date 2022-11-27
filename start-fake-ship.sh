74906de8f1e1a97f873a6ea96fc959378ad67fb0
bohendo <bohendo@pm.me>
2022-11-27 15:40:26 -0500

    update fake ship starter for quicker fresh restarts

--- start-fake-ship.sh
+++ start-fake-ship.sh
@@ -1,11 +1,22 @@
 #!/usr/bin/env bash
 
+name="zod"
+snapshot="fresh-zod"
+
 if [[ -n "$(command -v urbit)" ]]
 then
   echo "Starting urbit via local binary at $(realpath $(which urbit))"
   mkdir -p data/urbit
   cd data/urbit
-  urbit -F zod
+  if [[ -d "$snapshot" ]]
+  then
+    rm -rf "$name"
+    cp -r "$snapshot" "$name"
+    urbit zod
+  else
+    echo "Booting a new urbit, press ctrl-d immediately after it finishes booting then re-run this script to quickly restart from a fresh snapshot"
+    urbit -F "$name" -c "$snapshot"
+  fi
 elif [[ -n "$(command -v docker)" ]]
 then
   image="tloncorp/urbit:v1.12"
